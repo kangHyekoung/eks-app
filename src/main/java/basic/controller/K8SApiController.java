b748cd6648b544487c0e678918da9e8ed88a30cd
kangHyekoung <107085610+kangHyekoung@users.noreply.github.com>
2025-02-08 17:35:00 +0900

    Update K8SApiController.java

--- src/main/java/basic/controller/K8SApiController.java
+++ src/main/java/basic/controller/K8SApiController.java
@@ -54,4 +54,9 @@ public class K8SApiController {
 	public String k8sApiTest9() {
 		return "Status9 - returned by Pod";
 	}
+
+	@GetMapping(value = "/api/status10")
+	public String k8sApiTest10() {
+		return "Status10 - returned by Pod";
+	}
 }
